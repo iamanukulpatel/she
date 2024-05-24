939b8c07e956bdb33cbaa9aad5f94752c108e5e5
intellianukul <anukulkumar@intellithink.in>
2024-05-25 01:06:26 +0530

    2nd

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 var config = {
     lines: [
         "Dear Astha❤️,",
-        "i never knew how much you mean to me when we first started talking . until today,i still love you as much as i did since the very first day we talk. ",
-        " every since you came into my life, i felt like i was the happiest. i just want to let you know how much you mean to me and how much i appreciate you.",
-        "you never failed to make me smile. things aren't getting better every since we fought, i just miss you so much. having random break downs, with you on my mind wondering if you're doing fine. im sorry i wasn't what you wanted,",
-        "im sorry i wasn't the best, im sorry i couldn't be good enough for you.i just hope everything will be okay soon, i really miss how we used to talk.🥹 I love you a lot 💕",
+        "I never knew how much you mean to me when we first started talking. Until today, I still love you as much as I did since the very first day we talk. ",
+        "Every since you came into my life, I felt like I was the happiest. I just want to let you know how much you mean to me and how much i appreciate you.",
+        "You never failed to make me smile. things aren't getting better every since we fought, I just miss you so much. having random break downs, with you on my mind wondering if you're doing fine. I'm sorry I wasn't what you wanted,",
+        "I'm sorry I wasn't the best, im sorry I couldn't be good enough for you. I just hope everything will be okay soon, I really miss how we used to talk.🥹 I love you a lot 💕",
 
     ],
     date: "2024-05-22T20:12:00",
